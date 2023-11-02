eb11c1c5c196d8295abd3b81bcea9df38705a8df
rijarandria <rijandrisolo@gmail.com>
2023-11-02 15:16:40 +0100

    Constructeur TP02 fini 2

--- src/fr/entites/TestPersonne.java
+++ src/fr/entites/TestPersonne.java
@@ -2,11 +2,11 @@ package fr.entites;
 
 public class TestPersonne {
     public static void main(String[] args) {
+        AdressePostale adress1 = new AdressePostale(5,"Rue Rimbaud","34080","Montpellier");
+        AdressePostale adress2 = new AdressePostale(1,"Avenue des Mirtilles","34080","Montpellier");
+        Personne pers1 = new Personne("Dupont","Arsène", adress1);
 
-
-        Personne pers1 = new Personne("Dupont","Arsène",new  AdressePostale(5,"Rue Rimbaud","34080","Montpellier"));
-
-        Personne pers2 = new Personne("Lupin","Arsène",new  AdressePostale(5,"Rue Rimbaud","34080","Montpellier"));
+        Personne pers2 = new Personne("Lupin","Arsène",adress2);
 
 
 //System.out.println( pers1.nom +" "+pers1.prenom+" "+pers1.adresse.numeroRue);
