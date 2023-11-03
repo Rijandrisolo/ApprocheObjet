476db467e21274caec32562481d8df278efd06c4
rijarandria <rijandrisolo@gmail.com>
2023-11-03 17:26:26 +0100

    Tp08 calcul montant global

--- src/fr/diginamic/banque/entites/TestOperation.java
+++ src/fr/diginamic/banque/entites/TestOperation.java
@@ -2,8 +2,8 @@ package fr.diginamic.banque.entites;
 
 public class TestOperation {
     public static void main(String[] args) {
-        double cred = 0.00;
-        double deb = 0.00;
+
+        double montantGlobal = 0.00;
         Credit cred1 = new Credit("21/10/2023", 150.00);
         Credit cred2 = new Credit("03/11/2023", 52.00);
         Debit deb1 = new Debit("22/10/2023", 50.00);
@@ -14,9 +14,17 @@ public class TestOperation {
         tableau[1] = cred2;
         tableau[2] = deb1;
         tableau[3] = deb2;
+
         for (Operation tab : tableau) {
+            if( tab.GetType() =="Crédit"){
+                montantGlobal = montantGlobal+tab.getMontantOperation();
+            }else {
+                montantGlobal = montantGlobal-tab.getMontantOperation();
+            }
             System.out.println(tab+" "+tab.GetType());
+            System.out.println("Le Montant global est : "+ montantGlobal);
         }
+
 
     }
 }
