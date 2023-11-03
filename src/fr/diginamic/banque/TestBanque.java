179fb9463d87524197ad670a8893231ebc6a5af8
rijarandria <rijandrisolo@gmail.com>
2023-11-03 15:17:44 +0100

    tp07 héritage et constructeurs création des comptes normal et taux

--- src/fr/diginamic/banque/TestBanque.java
+++ src/fr/diginamic/banque/TestBanque.java
@@ -8,7 +8,17 @@ public class TestBanque {
         Compte cpt1= new Compte("dffdffdf",250.00 );
         CompteTaux cptTaux =new CompteTaux("FRessai34",450.00,1.00);
 
-        System.out.println(cptTaux);
+        Compte[] tableau = new Compte[2];
+        tableau[1] = cpt1;
+        tableau[0] = cptTaux;
+
+        for(int i=0;i<=tableau.length-1;i++){
+            System.out.println(tableau[i]);
+
+        }
+
+
+       // System.out.println(cptTaux);
 
     }
 }
