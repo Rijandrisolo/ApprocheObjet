3e962727c0d5d91e6702ceee6014fcdd331a74ea
rijarandria <rijandrisolo@gmail.com>
2023-11-03 14:11:21 +0100

    tp05 ajout toString

--- src/fr/diginamic/banque/TestBanque.java
+++ src/fr/diginamic/banque/TestBanque.java
@@ -4,11 +4,9 @@ import fr.diginamic.banque.entites.Compte;
 
 public class TestBanque {
     public static void main(String[] args) {
-        Compte cpt1= new Compte();
-        //Compte cpt2= new Compte();
-         cpt1.setNumCompte("dffdfdfdfdfdf");
-         cpt1.setSolde(250.00);
-        System.out.println(cpt1.getNumCompte() +" "+cpt1.getSolde());
+        Compte cpt1= new Compte("dffdffdf",250.00 );
+
+        System.out.println(cpt1);
 
     }
 }
--- src/fr/diginamic/banque/entites/Compte.java
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -4,12 +4,28 @@ public class Compte {
     private String numCompte;
     private double solde;
 
+
+    public  Compte(String vnumCompte, double vsolde){
+        this.numCompte = vnumCompte;
+        this.solde = vsolde;
+
+    }
+
+    @Override
+    public String toString() {
+        return
+                "numCompte='" + numCompte + '\'' +
+                ", solde=" + solde
+              ;
+    }
+
     public String getNumCompte() {
         return numCompte;
     }
 
     public void setNumCompte(String numCompte) {
         this.numCompte = numCompte;
+
     }
 
     public double getSolde() {
